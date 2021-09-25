e7913133a6ef1e0e9f90efdfad4393762ba75d57
oyl <npr05080@gmail.com>
2021-09-26 01:40:06 +0900

    fix: 권한 관련

--- board/src/main/java/com/oyl/board/exception/PostNotFoundException.java
+++ board/src/main/java/com/oyl/board/exception/PostNotFoundException.java
@@ -1,12 +1,6 @@
 package com.oyl.board.exception;
 
 public class PostNotFoundException extends RuntimeException{
-    public PostNotFoundException(String msg, Throwable t){
-        super(msg,t);
-    }
-    public PostNotFoundException(String msg){
-        super(msg);
-    }
     public PostNotFoundException(){
         super();
     }
--- board/src/main/java/com/oyl/board/post/PostService.java
+++ board/src/main/java/com/oyl/board/post/PostService.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.Iterator;
 
 
@@ -25,14 +26,16 @@ public class PostService {
 
     @Transactional
     public Post create(PostRequest request, Long boardId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Member member = (Member) authentication.getPrincipal();
         Board board = boardRepository.findByBoardId(boardId);
         return postRepository.save(Post.builder()
                 .board(board)
                 .content(request.getContent())
-                .nickname(request.getNickname())
+                .nickname(member.getName())
                 .title(request.getTitle())
-                .regDate(request.getRegDate())
-                .member(memberRepository.findByName(request.getNickname()))
+                .regDate(LocalDate.now())
+                .member(member)
                 .build());
     }
 
