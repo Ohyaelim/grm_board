6804d562e38e3e1de0b1444a338b885d3a3cd925
oyl <npr05080@gmail.com>
2021-09-26 18:04:03 +0900

    feat : FE 마이페이지 틀 잡기

--- board-vue/src/views/Login.vue
+++ board-vue/src/views/Login.vue
@@ -47,6 +47,14 @@
                   // password: password 이멜과 패스워드를 store.js의 signinObj로 넘기겠다
                   password,
                 })"
+                @keyup.enter="
+                login({
+                  // email: email,
+                  email,
+                  // password: password 이멜과 패스워드를 store.js의 signinObj로 넘기겠다
+                  password,
+                })
+              "
             >
               로그인</v-btn>
           </div>
